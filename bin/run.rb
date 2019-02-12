b9a7a5714cd31718c0d4e1096669f161f39a2aa3
Ethan Nam <ethan.s.nam@gmail.com>
2019-02-12 15:35:48 -0500

    Proof of concept for api-call

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,6 @@
 require_relative '../config/environment.rb'
 require_relative '../bin/import_stations.rb'
+require_relative '../bin/api_call.rb'
 
 puts "Hello, world. Welcome to Subway Times."
 binding.pry
