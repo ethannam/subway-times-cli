609111bd25600b2f5d577fab394f27566e5327d2
Farhan <farhanmahmoodny@gmail.com>
2019-02-12 14:04:42 -0500

    parse station info

--- app/models/line.rb
+++ app/models/line.rb
@@ -6,7 +6,9 @@ class Line < ActiveRecord::Base
     LineStation.create(station_id: station.id, line_id: self.id, status: status)
   end
 
-  # def add_train(train)
-  #   self.train_id = train.id
-  # end
+  # station_name = import_stations[0][10]
+  # station_lat = import_stations[0][11].slice(-18,17)
+  # station_long = import_stations[0][11].slice(7,18)
+  # station_lines = import_stations[0][12].split(" ")[0].split("-").uniq
+  
 end
