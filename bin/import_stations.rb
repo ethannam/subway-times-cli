88ec8615c1da0158aa1814617a553a71f920376b
Ethan Nam <ethan.s.nam@gmail.com>
2019-02-12 14:57:46 -0500

    Import stations method finished.

--- bin/import_stations.rb
+++ bin/import_stations.rb
@@ -7,12 +7,30 @@ def import_stations
   stations_array = result_hash["data"]
 
   # Create a hash to get information we care about
-  stations_array.inject({}) do |hash, station|
+  our_hash = stations_array.inject({}) do |hash, station|
     name = station[10]
     latitude = station[11].slice(-18,17)
     longitude = station[11].slice(7,18)
     lines = station[12].split(" ")[0].split("-").uniq
-    hash[name] = {latitude: latitude, longitude: longitude, lines: lines}
+
+    # hash[name] = {latitude: latitude, longitude: longitude, lines: lines}
+
+    if hash[name]
+      hash[name][:lines] = hash[name][:lines] + lines
+    else
+      hash[name] = {latitude: latitude, longitude: longitude, lines: lines}
+    end
+    
     hash
+  end
+
+  # Iterate through the hash and create the stations and associate them with lines
+  our_hash.each do |station, attributes|
+    new_station = Station.create(name: station, latitude: attributes[:latitude], longitude: attributes[:longitude])
+
+    attributes[:lines].each do |line|
+      current_line = Line.find_by(name: line)
+      current_line.add_station(new_station, "Running")
+    end
   end
 end
