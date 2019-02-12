ada382053adede3b865d10a92def542aa81a753d
Ethan Nam <ethan.s.nam@gmail.com>
2019-02-12 14:27:02 -0500

    Created a hash to get information we care about.

--- app/models/line.rb
+++ app/models/line.rb
@@ -5,10 +5,5 @@ class Line < ActiveRecord::Base
   def add_station(station, status)
     LineStation.create(station_id: station.id, line_id: self.id, status: status)
   end
-
-  # station_name = import_stations[0][10]
-  # station_lat = import_stations[0][11].slice(-18,17)
-  # station_long = import_stations[0][11].slice(7,18)
-  # station_lines = import_stations[0][12].split(" ")[0].split("-").uniq
   
 end
--- bin/import_stations.rb
+++ bin/import_stations.rb
@@ -1,5 +1,19 @@
+require 'rest-client'
+
 def import_stations
-  result_string = File.read(File.expand_path("../../json/subway_stations.json", __FILE__))
+  # Get stations array
+  # result_string = File.read(File.expand_path("../../json/subway_stations.json", __FILE__))
+  result_string = RestClient.get('https://data.cityofnewyork.us/api/views/kk4q-3rt2/rows.json?accessType=DOWNLOAD')
   result_hash = JSON.parse(result_string)
   stations_array = result_hash["data"]
+
+  # Create a hash to get information we care about
+  stations_array.inject({}) do |hash, station|
+    name = station[10]
+    latitude = station[11].slice(-18,17)
+    longitude = station[11].slice(7,18)
+    lines = station[12].split(" ")[0].split("-").uniq
+    hash[name] = {latitude: latitude, longitude: longitude, lines: lines}
+    hash
+  end
 end
