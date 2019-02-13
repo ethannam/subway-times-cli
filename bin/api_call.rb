8ed3eeb1779e921f47dafcfd0ee208da6474627b
Ethan Nam <ethan.s.nam@gmail.com>
2019-02-13 15:46:58 -0500

    Brought over api functionality plus clean db

---
+++ bin/api_call.rb
@@ -0,0 +1,30 @@
+require 'httparty'
+require 'protobuf'
+require 'google/transit/gtfs-realtime.pb'
+require 'net/http'
+require 'uri'
+
+def url
+  # query = {key: ENV.fetch("MTA_KEY"), feed_id: mta_id}.to_query
+  # "http://datamine.mta.info/mta_esi.php?#{query}"
+
+  query = {key: 'dd542468b31829a77225d110c37bbdcf', feed_id: 1}.to_query
+  "http://datamine.mta.info/mta_esi.php?#{query}"
+end
+
+# def fetch_data
+#   data = HTTParty.get(url).body
+#   TransitRealtime::FeedMessage.decode(data)
+# end
+
+def api_call
+  data = Net::HTTP.get(URI.parse(url))
+  feed = Transit_realtime::FeedMessage.decode(data)
+  binding.pry
+  for entity in feed.entity do
+    binding.pry
+    if entity.field?(:trip_update)
+      p entity.trip_update
+    end
+  end
+end
--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,7 @@
 require_relative '../config/environment.rb'
 require_relative '../lib/station_importer.rb'
 require_relative '../lib/line_station_importer.rb'
+require_relative '../bin/api_call.rb'
 
 puts "Hello, world. Welcome to Subway Times."
 
